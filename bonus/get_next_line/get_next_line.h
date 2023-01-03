cc72457b67c64bfcb3f9143fdadbb44295c7220b
Lahbib Semlali <lsemlali@e1r1p10.1337.ma>
2023-01-03 07:55:48 +0100

    great v

---
+++ bonus/get_next_line/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsemlali <lsemlali@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/09 19:57:34 by lsemlali          #+#    #+#             */
+/*   Updated: 2023/01/03 07:53:17 by lsemlali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# define BUFFER_SIZE 1
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+int		ft_search(const char *s);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+void	ft_cut(char *buff);
+
+#endif
